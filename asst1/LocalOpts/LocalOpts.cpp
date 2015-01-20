2db451ec0cba8bf44f6bc8394c5baa81ac09dce0
Nisarg Shah <nisarg89@gmail.com>
2015-01-20 00:45:27 -0500

    Added more comments

--- asst1/LocalOpts/LocalOpts.cpp
+++ asst1/LocalOpts/LocalOpts.cpp
@@ -13,7 +13,10 @@
 using namespace std;
 using namespace llvm;
 
+// Get constant from value
 #define get_const(t,val) ConstantInt::get((IntegerType*)t,val)
+
+// DEBUG mode: Prints all the optimizations performed to stdout, and the original and final code so we can compare!
 #define DEBUG 1
 
 #ifdef DEBUG
@@ -24,7 +27,7 @@ using namespace llvm;
 
 namespace
 {
-	
+	// Struct storing statistics about the number of different optimizations performed
 	struct LocalOptsInfoStruct {
 		LocalOptsInfoStruct() : numAlgebraicOpts(0), numConstantFolds(0), numStrengthReds(0) {}
 		int numAlgebraicOpts;
@@ -46,20 +49,20 @@ namespace
 			// Iterate over all instructions
 			for(BasicBlock::iterator BI = B.begin(), BE = B.end(); BI != BE; ) {
 				Instruction& inst(*BI);
-				bool inst_removed = false;
+				bool inst_removed = false;	// Is this instruction removed due to constant folding?
 				
 				// In binary operations
 				if(BinaryOperator::classof(&inst)) {
 					BinaryOperator *bop((BinaryOperator*)&inst);
-					Value *val1(bop->getOperand(0));
+					Value *val1(bop->getOperand(0));	// Get the first and the second operand (as values)
 					Value *val2(bop->getOperand(1));
-					if(!ConstantInt::classof(val1) || !ConstantInt::classof(val2)) continue; // The values are not integers
-					ConstantInt *ci1 = dyn_cast<ConstantInt>(val1);
+					if(!ConstantInt::classof(val1) || !ConstantInt::classof(val2)) continue; // The values are not integers, then there's nothing to do!
+					ConstantInt *ci1 = dyn_cast<ConstantInt>(val1);	// Get constants from values
 					ConstantInt *ci2 = dyn_cast<ConstantInt>(val2);
 
-					switch (bop->getOpcode()) {
+					switch (bop->getOpcode()) {	// Fold depending on what operator is used
 					
-					case Instruction::Add: {
+					case Instruction::Add: {	// Addition
 						ConstantInt *ci_final(get_const(ci1->getType(), (ci1->getSExtValue()) + (ci2->getSExtValue())));
 						DBG(outs() << "ConstantFolding: " << ci1->getSExtValue() << " + " << ci2->getSExtValue() << " = " << ci_final->getSExtValue() << "\n");
 						ReplaceInstWithValue(B.getInstList(),BI,ci_final);
@@ -68,7 +71,7 @@ namespace
 						}
 						break;
 
-					case Instruction::Sub: {
+					case Instruction::Sub: {	// Subtraction
 						ConstantInt *ci_final(get_const(ci1->getType(), (ci1->getSExtValue()) - (ci2->getSExtValue())));
 						DBG(outs() << "ConstantFolding: " << ci1->getSExtValue() << " - " << ci2->getSExtValue() << " = " << ci_final->getSExtValue() << "\n");
 						ReplaceInstWithValue(B.getInstList(),BI,ci_final);
@@ -77,7 +80,7 @@ namespace
 						}
 						break;
 						
-					case Instruction::Mul: {
+					case Instruction::Mul: {	// Multiplication
 						ConstantInt *ci_final(get_const(ci1->getType(), (ci1->getSExtValue()) * (ci2->getSExtValue())));
 						DBG(outs() << "ConstantFolding: " << ci1->getSExtValue() << " * " << ci2->getSExtValue() << " = " << ci_final->getSExtValue() << "\n");
 						ReplaceInstWithValue(B.getInstList(),BI,ci_final);
@@ -86,7 +89,7 @@ namespace
 						}
 						break;
 
-					case Instruction::SDiv: {
+					case Instruction::SDiv: {	// Division
 						ConstantInt *ci_final(get_const(ci1->getType(), (ci1->getSExtValue()) / (ci2->getSExtValue())));
 						DBG(outs() << "ConstantFolding: " << ci1->getSExtValue() << " / " << ci2->getSExtValue() << " = " << ci_final->getSExtValue() << "\n");
 						ReplaceInstWithValue(B.getInstList(),BI,ci_final);
@@ -95,7 +98,7 @@ namespace
 						}
 						break;
 
-					case Instruction::Shl: {
+					case Instruction::Shl: {	// Left Shift
 						ConstantInt *ci_final(get_const(ci1->getType(), (ci1->getSExtValue()) << (ci2->getSExtValue())));
 						DBG(outs() << "ConstantFolding: " << ci1->getSExtValue() << " << " << ci2->getSExtValue() << " = " << ci_final->getSExtValue() << "\n");
 						ReplaceInstWithValue(B.getInstList(),BI,ci_final);
@@ -104,7 +107,7 @@ namespace
 						}
 						break;
 
-					case Instruction::LShr: {
+					case Instruction::LShr: {	// (Logical) Right Shift
 						ConstantInt *ci_final(get_const(ci1->getType(), (ci1->getSExtValue()) >> (ci2->getSExtValue())));
 						DBG(outs() << "ConstantFolding: " << ci1->getSExtValue() << " >> " << ci2->getSExtValue() << " = " << ci_final->getSExtValue() << "\n");
 						ReplaceInstWithValue(B.getInstList(),BI,ci_final);
@@ -115,7 +118,9 @@ namespace
 					}
 				}
 				
-				if (!inst_removed) ++BI;
+				// Increment the iterator only if the current instruction was not removed. 
+				// This is chosen instead of doing --BI when we remove the instruction because --BI crashes when BI is at B.begin()
+				if (!inst_removed) ++BI;	
 			}
 		}
 
@@ -158,22 +163,30 @@ namespace
 
 				for(Function::iterator FI = F.begin(), FE = F.end(); FI != FE; ++FI) {
 					BasicBlock& B(*FI);
+					
+					// In the debug mode, print the original code to stdout
 					DBG(outs() << "ORIGINAL CODE:\n\n");
 					DBG(B.dump());
+					
+					// In the debug mode, print every optimization performed to stdout
 					DBG(outs() << "\nOPTIMIZATIONS PERFORMED:\n\n");
 					algebraic_optimizations(B);
 					constant_folding(B);
 					strength_reduction(B);
+					
+					// In the debug mode, print the final code to stdout
 					DBG(outs() << "\nFINAL CODE:\n\n");
 					DBG(B.dump());
 				}
 			}
 			
+			// Print the summary statistics to stdout
 			printLocalOptsSummary();
 			return true;
 		}
 	};
 
+	// Register the pass
 	char LocalOpts::ID = 0;
 	RegisterPass<LocalOpts> X("local-opts", "15745: Local Optimizations");
 }
