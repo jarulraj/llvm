678ad434cb8e0fa80db940da5dd87121045e577e
jarulraj <jarulraj@cs.cmu.edu>
2015-04-12 18:51:20 -0400

    Annotations are working now.

--- project/src/LoopMemoryAnalysis.cpp
+++ project/src/LoopMemoryAnalysis.cpp
@@ -1,6 +1,8 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"             
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopPass.h"
 #include "llvm/Analysis/ScalarEvolution.h"
@@ -10,26 +12,43 @@
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/ScalarEvolutionExpressions.h"
 
+#include <vector>
+#include <algorithm> 
+
 using namespace llvm;
 using namespace std;
 
 namespace {
-    class LoopMemoryAnalysis : public LoopPass {
+    StringRef getGlobalStringConstant(Value *val) {
+        if (val->getValueID() != Value::ConstantExprVal) {
+            outs() << val->getValueID() << "\t" << Value::ConstantExprVal;
+            return StringRef();
+        }
+        ConstantExpr *ce = (ConstantExpr *)val;
+        GlobalVariable *gv = (GlobalVariable *)ce->getOperand(0);
+        return cast<ConstantDataArray>(gv->getInitializer())->getAsString();
+    }
+
+    class LoopMemoryAnalysis : public FunctionPass {
         public:
             static char ID;
 
-            LoopMemoryAnalysis() : LoopPass(ID) {}
+            LoopMemoryAnalysis() : FunctionPass(ID) {}
 
             ScalarEvolution  *SE;
 
-            void getAnalysisUsage(AnalysisUsage &AU) const override {
-                  AU.addRequired<ScalarEvolution>();
+            // Interested data structures
+            std::vector<Value*> structures;
 
-                  AU.setPreservesCFG();
-                  AU.addPreserved<ScalarEvolution>();
+            void getAnalysisUsage(AnalysisUsage &AU) const override {
+                AU.addRequired<ScalarEvolution>();
+                AU.addRequired<LoopInfo>();
+
+                AU.setPreservesCFG();
+                AU.addPreserved<ScalarEvolution>();
             }
 
-            virtual bool runOnLoop(Loop *L, LPPassManager &LPM) {
+            virtual bool runOnLoop(Loop *L) {
 
                 bool changed = false;
 
@@ -49,17 +68,28 @@ namespace {
                             SI->print(outs());
                             outs() << "\n";
 
-                            Value *operand = SI->getPointerOperand();
+                            const Value *pointer_operand = SI->getPointerOperand();
 
                             // GetElementPtr Instruction
-                            if (const GEPOperator *GEP = dyn_cast<GEPOperator>(operand)) {
+                            if (const GEPOperator *GEP = dyn_cast<GEPOperator>(pointer_operand)) {
+
+                                const Value* pointer_operand = GEP->getPointerOperand();
+                                
+                                if(std::count(structures.begin(), structures.end(), pointer_operand) == 0)
+                                    continue;
+                                
+                                outs() << "Pointer : ";
+                                pointer_operand->print(outs());
+                                outs() << "\n";
+
+
                                 for (GEPOperator::const_op_iterator idx = GEP->idx_begin(), end = GEP->idx_end(); idx != end; idx++) {
                                     const SCEV *index = SE->getSCEV(*idx);
 
                                     outs() << "Index : ";
                                     index->print(outs());
                                     outs() << "\n";
-                            
+
                                     if (const SCEVAddRecExpr *addRec = dyn_cast<SCEVAddRecExpr>(index)) {
                                     }
                                 }
@@ -79,6 +109,61 @@ namespace {
 
                 return changed;
             }
+
+            virtual bool runOnFunction(Function &F) {
+                if (F.size() == 0)
+                    return false;
+                
+                outs() << F.getName() << ":\n\n";
+                
+                // ANNOTATION ANALYSIS
+
+                // Annotations always in the entry block
+                std::map<Value *, Value *> mapValueToArgument;
+                BasicBlock *b = &F.getEntryBlock();
+
+                //Build pointer to value mapping
+                for(BasicBlock::iterator it = b->begin(); it!=b->end();++it) {
+                    Instruction *inst = it;
+                    if (inst->getOpcode()!=Instruction::BitCast) {
+                        continue;
+                    }
+
+                    mapValueToArgument[inst] = inst->getOperand(0);
+                }
+
+                for (auto it = b->begin(); it != b->end(); it++) {
+                    Instruction *I = it;
+                    if (I->getOpcode() != Instruction::Call) {
+                        continue;
+                    }
+                    Value *calledFunction = I->getOperand(I->getNumOperands()-1);
+                    if(calledFunction->getName().str() != "llvm.var.annotation")
+                        continue;
+                    Value * annotatedValue = I->getOperand(0);
+                    if (mapValueToArgument.count(annotatedValue)) {
+                        annotatedValue = mapValueToArgument[annotatedValue];
+                    }
+                    
+                    Value *annotation = I->getOperand(1);
+                    Value *filename = I->getOperand(2);
+                    Value *linenumber = I->getOperand(3);
+                   
+                    outs() << getGlobalStringConstant(filename) << ":" << *linenumber << " " <<
+                        "\t" << *annotatedValue << "\t" << getGlobalStringConstant(annotation) << "\n";
+
+                    structures.push_back(annotatedValue);
+                }
+
+                // LOOP ANALYSIS
+                LoopInfo &LI = getAnalysis<LoopInfo>();
+                for (LoopInfo::iterator loop_itr = LI.begin(), e = LI.end(); loop_itr != e; ++loop_itr) {
+                    runOnLoop(*loop_itr);
+                }
+
+                return false;
+            }
+
     };
 }
 
--- project/src/tests/simple.c
+++ project/src/tests/simple.c
@@ -6,8 +6,8 @@ int main() {
     int i;
     __attribute__((annotate("matrix annotation"))) char a[10];
     __attribute__((annotate("annotate 2"))) char *b = a;
-
     __attribute__((annotate("annotate 3"))) int *c = malloc(sizeof(int) * 24);
+    
     int j;
     int sum = 0;
     int n = 23;
@@ -16,20 +16,20 @@ int main() {
         j = i + 2;
         a[j] = i * j;
     }
-/*
-    int m[20][20];
+    /*
+       int m[20][20];
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < 100; j++){
-            m[i][j] = 0;
-            m[j][i] = 0;
+       for (int i = 0; i < n; i++)
+       for (int j = 0; j < 100; j++){
+       m[i][j] = 0;
+       m[j][i] = 0;
 
-            a[i*n+j] = 0;
-            a[j*n+i] = 0;
+       a[i*n+j] = 0;
+       a[j*n+i] = 0;
 
-            a[2*i+23] = 0;
-        }
-*/
+       a[2*i+23] = 0;
+       }
+       */
     printf("sum : %d\n", sum);
 
     return 0;
