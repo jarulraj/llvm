a684e309fb1f91e6e78facc5f0e6c99a04f99e5a
jarulraj <jarulraj@cs.cmu.edu>
2015-04-12 20:10:10 -0400

    Trying to delinearize SCEV to do analysis

--- project/src/tests/simple.c
+++ project/src/tests/simple.c
@@ -4,7 +4,8 @@ int main() {
 
 
     int i;
-    __attribute__((annotate("matrix annotation"))) char a[10];
+    //__attribute__((annotate("matrix annotation"))) char a[10];
+    char a[10];
     __attribute__((annotate("hey, this is important"))) int m[20][20];
     
     int j;
@@ -13,13 +14,15 @@ int main() {
 
     for(i = 0 ; i < 100 ; i++) {
         j = i + 2;
-        //a[j] = i * j;
+        a[j] = i * j;
     }
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < 100; j++){
             m[i][j] = 0;
             m[j][i] = 0;
+
+            m[3][2] = 5;
 
             a[i*n+j] = 0;
             a[j*n+i] = 0;
