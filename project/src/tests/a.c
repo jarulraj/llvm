176be8a1e7fdc5fa23d33e1ecb0c7ae84f193550
jarulraj <jarulraj@cs.cmu.edu>
2015-04-14 17:12:49 -0400

    Updated test case to use annotations

--- project/src/tests/a.c
+++ project/src/tests/a.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 
-void foo(long n, long m, int A[n][m]) {
+void foo(long n, long m)  {
+    __attribute__((annotate("hey, this is important"))) int A[n][m];
 
-    for (long i = 0; i < n; i++)
+    for (long i = 0; i < n; i++) {
         for (long j = 0; j < m; j++){
-                A[i][j] = 0;
-                A[j][i] = 0;
+            A[i][j] = 0;
+            A[j][i] = 0;
+
+            A[2*i+j][i-5*j] = 1;
         }
+    }
 
 }
