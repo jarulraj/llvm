fbdb3958a3b496183a9bea7b477dcdcfe477507f
jarulraj <jarulraj@cs.cmu.edu>
2015-04-14 14:58:41 -0400

    Simplified

--- project/src/tests/simple.c
+++ project/src/tests/simple.c
@@ -6,12 +6,12 @@ int main() {
     int i;
     //__attribute__((annotate("matrix annotation"))) char a[10];
     char a[10];
-    __attribute__((annotate("hey, this is important"))) char b[128][256];
+    __attribute__((annotate("hey, this is important"))) char b[32][64];
 
     int j;
     int sum = 0;
-    int m = 128;
-    int n = 256;
+    int m = 10;
+    int n = 30;
 
     for(i = 0 ; i < 100 ; i++) {
         j = i + 2;
@@ -20,8 +20,8 @@ int main() {
 
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++){
-            b[i][j] = 0;
-            b[j][i] = 0;
+            b[i][j+5] = 0;
+            //b[j][i] = 0;
 
             //a[i*n+j] = 0;
             //a[j*n+i] = 0;
