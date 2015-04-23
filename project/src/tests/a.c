2612edd57f358a51d21b904849fb9537f6f55fd5
jarulraj <jarulraj@cs.cmu.edu>
2015-04-23 17:35:47 -0400

    Added more tests

--- project/src/tests/a.c
+++ project/src/tests/a.c
@@ -12,13 +12,25 @@ void foo(long n, long m)  {
     __attribute__((annotate("hey, keys"))) struct key keys[len]; 
 
     char x;
-    int i, j;
+    int i, j, k;
 
     for (i = 0; i < len ; i++) {
         for (j = 0; j < len ; j++) {
             x = keys[i].data[j];
             x = keys[j].data[i];
             x = keys[i].data[j];
+        }
+    }
+
+    for(k = 0 ; k < 100 ; k++) { 
+        for (i = 0; i < n; i+=2) {
+            for (j = 0; j < m; j++){
+                A[i][j] = 0;
+                A[j][i] = 0;
+
+                x = keys[i].data[0];
+                keys[i].data[1] = x;
+            }
         }
     }
 
